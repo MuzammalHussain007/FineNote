dd4b0e0f86727c60d477504918b044b44168b844
Muzammal Hussain <59975423+MuzammalHussain007@users.noreply.github.com>
2022-07-19 18:14:44 +0500

    new changes

--- app/src/main/java/com/practice/finenote/MainActivity.kt
+++ app/src/main/java/com/practice/finenote/MainActivity.kt
@@ -2,10 +2,35 @@ package com.practice.finenote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.practice.finenote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navCotroller: NavController;
+    private lateinit var binding: ActivityMainBinding;
+    private lateinit var appBarConfiguration: AppBarConfiguration;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
+        navCotroller = navHostFragment.findNavController()
+        setSupportActionBar(binding.toolBar)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.loginFragment)
+        )
+        setupActionBarWithNavController(navCotroller, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navCotroller.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/practice/finenote/fragment/user/SignUpFragment.kt
+++ app/src/main/java/com/practice/finenote/fragment/user/SignUpFragment.kt
@@ -6,12 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.practice.finenote.R
+import com.practice.finenote.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        innit(view)
+    }
+
+    private fun innit(view: View) {
+      binding = FragmentSignUpBinding.bind(view)
+
     }
 
 

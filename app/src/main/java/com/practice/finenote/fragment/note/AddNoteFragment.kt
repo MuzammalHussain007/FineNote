a511873c670bd13866e4fe7bd46cf4de69caebff
Muzammal Hussain <59975423+MuzammalHussain007@users.noreply.github.com>
2022-07-29 16:51:13 +0500

    New Changes

--- app/src/main/java/com/practice/finenote/fragment/note/AddNoteFragment.kt
+++ app/src/main/java/com/practice/finenote/fragment/note/AddNoteFragment.kt
@@ -2,8 +2,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.practice.finenote.R
+import com.practice.finenote.fragment.BaseFragment
 
-class AddNoteFragment : Fragment(R.layout.fragment_add_note) {
+class AddNoteFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/practice/finenote/modals/dbmodal/RoomDataBaseUserModal.kt
+++ app/src/main/java/com/practice/finenote/modals/dbmodal/RoomDataBaseUserModal.kt
@@ -8,8 +8,8 @@ private const val TABLENAME = "User"
 @Entity(tableName = TABLENAME)
  class RoomDataBaseUserModal(
  @PrimaryKey(autoGenerate = true)
- @ColumnInfo(name = "UserID") private val userid : Int ,
- @ColumnInfo(name = "Username") private val username : String ,
- @ColumnInfo(name = "Email") private val email : String ,
- @ColumnInfo(name = "Password") private val password : String ,
+   val userid : Int ,
+ @ColumnInfo(name = "Username")  val username : String ,
+ @ColumnInfo(name = "Email")  val email : String ,
+ @ColumnInfo(name = "Password")  val password : String ,
 )

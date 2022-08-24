ab5497864c8dcf2d99de9f6788ce02005b142ce6
Muzammal Hussain <muzammalhussain501@gmail.com>
2022-08-24 21:33:08 +0500

    New Feature Added

--- app/src/main/java/com/practice/finenote/di/NetworkModule.kt
+++ app/src/main/java/com/practice/finenote/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.practice.finenote.di
 import android.content.Context
 import com.practice.finenote.api.NoteAPI
 import com.practice.finenote.api.UserAPI
+import com.practice.finenote.utils.AuthenticationInterceptor
 import com.practice.finenote.utils.Constants.BASE_URL
 import com.readystatesoftware.chuck.ChuckInterceptor
 import dagger.Module
@@ -26,27 +27,35 @@ class NetworkModule {
     fun provideRetrofit( @ApplicationContext context: Context) : Retrofit.Builder{
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
-            .client(getHttpClient(context).build())
             .baseUrl(BASE_URL)
     }
 
 
-    private fun getLogging() : HttpLoggingInterceptor {
-        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-    }
-
-    private fun getHttpClient(  context : Context) : OkHttpClient.Builder {
-        return OkHttpClient.Builder().addInterceptor(getLogging()).addInterceptor(ChuckInterceptor(context))
-    }
+//    private fun getLogging() : HttpLoggingInterceptor {
+//        return HttpLoggingInterceptor()
+//            .setLevel(HttpLoggingInterceptor.Level.BODY)
+//    }
+//
+//    private fun getHttpClient(  context : Context) : OkHttpClient.Builder {
+//        return OkHttpClient
+//            .Builder()
+//            .addInterceptor(getLogging())
+//            .addInterceptor(ChuckInterceptor(context))
+//    }
     @Singleton
     @Provides
     fun  manageServiceUser(retrofit: Retrofit.Builder) : UserAPI {
-        return retrofit.build().create(UserAPI::class.java)
+        return retrofit
+            .build()
+            .create(UserAPI::class.java)
     }
 
-    @Singleton
-    @Provides
-    fun  manageServiceNote(retrofit: Retrofit.Builder) : NoteAPI {
-        return retrofit.build().create(NoteAPI::class.java)
-    }
+//    @Singleton
+//    @Provides
+//    fun  manageServiceNote(retrofit: Retrofit.Builder,@ApplicationContext context: Context,authenticationInterceptor: AuthenticationInterceptor) : NoteAPI {
+//        return retrofit.
+//        client(getHttpClient(context).addInterceptor(authenticationInterceptor).build())
+//            .build()
+//            .create(NoteAPI::class.java)
+//    }
 }

8b310c7f7957919e2aa3321b7a9f1735eb0f5a67
Muzammal Hussain <muzammalhussain501@gmail.com>
2022-08-25 23:57:07 +0500

    new changes

--- app/src/main/java/com/practice/finenote/fragment/note/AddNoteFragment.kt
+++ app/src/main/java/com/practice/finenote/fragment/note/AddNoteFragment.kt
@@ -1,14 +1,39 @@
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
-import com.practice.finenote.R
-import com.practice.finenote.fragment.BaseFragment
+package com.practice.finenote.fragment.note
 
-class AddNoteFragment : BaseFragment() {
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.practice.finenote.api.ErrorHandling
+import com.practice.finenote.databinding.FragmentHomeBinding
+import com.practice.finenote.databinding.FragmentLoginBinding
+import com.practice.finenote.fragment.BaseFragment
+import com.practice.finenote.modals.UserRequest
+import com.practice.finenote.utils.TokenManager
+import com.practice.finenote.viewModal.UserViewModal
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class HomeFragment : BaseFragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
-
 
 }

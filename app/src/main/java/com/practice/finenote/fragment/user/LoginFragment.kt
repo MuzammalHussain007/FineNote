48ba105b3fe8ec56ee8c1b07d919e16ff7fde640
Muzammal Hussain <muzammalhussain501@gmail.com>
2022-08-26 00:24:35 +0500

    new changes

--- app/src/main/java/com/practice/finenote/fragment/user/LoginFragment.kt
+++ app/src/main/java/com/practice/finenote/fragment/user/LoginFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Patterns
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.practice.finenote.R
 import com.practice.finenote.api.ErrorHandling
 import com.practice.finenote.databinding.FragmentLoginBinding
 import com.practice.finenote.fragment.BaseFragment
@@ -29,6 +31,7 @@ import javax.inject.Inject
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         if (tokenManager.getToken()==""){
 
         }else
@@ -43,6 +46,14 @@ import javax.inject.Inject
         super.onViewCreated(view, savedInstanceState)
         listener()
 
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        menu.findItem(R.id.deleteNote).isVisible = false
+        menu.findItem(R.id.theme).isVisible = false
+        menu.findItem(R.id.language).isVisible = false
+        menu.findItem(R.id.logout).isVisible = false
     }
     private fun observeApiData(view: View) {
         authenticationViewModal.userResponseLiveData.observe(viewLifecycleOwner) { it ->
